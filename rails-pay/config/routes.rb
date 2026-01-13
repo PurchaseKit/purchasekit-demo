d601eccc27eff891c2da0123ef70329a9f59f739
Joe Masilotti <joe@masilotti.com>
2026-01-13 12:28:07 -0800

    Mount engine correctlty

--- rails-pay/config/routes.rb
+++ rails-pay/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Mount PurchaseKit engine (auto-integrates with Pay when present)
-  mount PurchaseKit::Engine, at: "/purchase_kit"
+  mount PurchaseKit::Engine, at: "/purchasekit"
 
   resource :session, only: [:new, :create, :destroy]
   resource :dashboard, only: :show
